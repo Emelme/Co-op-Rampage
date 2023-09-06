afe610a3d662acf3a3c92898bfb85232a1503e78
Emelme <125494917+Emelme@users.noreply.github.com>
2023-09-06 15:34:04 +0200

    Add NavMesh, AI Navigation and Enemy Following

--- Co-op Rampage/Assets/Scripts/Enemy.cs
+++ Co-op Rampage/Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -14,13 +15,26 @@ public class Enemy : MonoBehaviour
 	private float timer;
 	private float timerMax = 2f;
 
+	public float distanceX;
+	public float distanceY;
+
+	private NavMeshAgent agent;
+
 	private void Start()
 	{
 		player = FindAnyObjectByType<PlayerBehaviour>().gameObject;
+
+		agent = GetComponent<NavMeshAgent>();
+		agent.updateRotation = false;
+		agent.updateUpAxis = false;
+		agent.speed = ed.speed;
 	}
 
 	private void Update()
 	{
+		distanceX = Mathf.Abs(transform.position.x - player.transform.position.x);
+		distanceY = Mathf.Abs(transform.position.y - player.transform.position.y);
+
 		if (!isPlayerDetected)
 		{
 			timer += Time.deltaTime;
@@ -36,9 +50,18 @@ public class Enemy : MonoBehaviour
 			isPlayerDetected = true;
 		}
 
-		if (isPlayerDetected)
+		if (isPlayerDetected && (distanceY > 1f || distanceX > 1.5f))
 		{
-			transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, player.transform.position.y + 0.5f), ed.speed * Time.deltaTime); 
+			//transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, player.transform.position.y + 0.5f), ed.speed * Time.deltaTime); 
+
+			Vector2 target = new(player.transform.position.x, player.transform.position.y + 0.7f);
+
+			agent.isStopped = false;
+			agent.SetDestination(target);
+		}
+		else if (distanceY < 1f || distanceX < 1.5f)
+		{
+			agent.isStopped = true;
 		}
 	}
 }
